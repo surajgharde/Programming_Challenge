17d00e522faa5db950fc3acae229c00a8b53f5db
surajgharde <gharadesuraj@gmail.com>
2025-08-15 22:29:27 +0530

    Sort 0,1,2

---
+++ 01_Dayone.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int low =0 , mid = 0, high = n-1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums[low],nums[mid]);
+                mid++; low++;
+            } else if (nums [mid] ==1 ) {
+                mid++;
+            }else {
+                swap(nums[high],nums[mid]);
+                high--;
+            }
+        }
+    }
+};
+
+
+int main () {
+    vector<int> nums = {2, 0, 2, 1, 1, 0};
+    
+
+    Solution obj;
+    obj.sortColors(nums);
+
+    cout << "Sorted array: ";
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
